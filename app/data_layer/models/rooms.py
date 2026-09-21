c27cf687c8070e5a94860ee18008a859d7d7b158
RomesAll <romesky845@gmail.com>
2026-09-21 20:59:38 +0300

    feat(#5): добавил orm модель для хранения участников комнат и связи relationship для них

--- app/data_layer/models/rooms.py
+++ app/data_layer/models/rooms.py
@@ -1,10 +1,20 @@
 from typing import TYPE_CHECKING
 from sqlalchemy import ForeignKey, String
 from sqlalchemy.orm import mapped_column, Mapped, relationship
+from enum import Enum
+from uuid import UUID
 from .base import Base, UuidIsMixin
 
 if TYPE_CHECKING:
     from .users import User
+    from .chats import Chat
+
+
+class RoomRole(str, Enum):
+    """Перечисление ролей в комнатах"""
+    OWNER = 'owner'
+    ADMIN = 'admins'
+    MEMBER = 'member'
 
 
 class Room(UuidIsMixin, Base):
@@ -14,4 +24,15 @@ class Room(UuidIsMixin, Base):
     avatar_url: Mapped[str] = mapped_column(default=None, nullable=True)
     owner_id: Mapped[str] = mapped_column(ForeignKey('user.id', ondelete='CASCADE'))
     is_private: Mapped[bool] = mapped_column(default=True)
-    user: Mapped['User'] = relationship(back_populates='rooms')
+    owner: Mapped['User'] = relationship(foreign_keys=[owner_id], back_populates='owned_rooms')
+    members: Mapped[list['RoomMember']] = relationship(back_populates='room', cascade='all, delete-orphan',)
+    chat: Mapped['Chat'] = relationship(back_populates='room')
+
+
+class RoomMember(Base):
+    """Orm модель для хранения информации об участниках комнаты"""
+    room_id: Mapped[UUID] = mapped_column(ForeignKey('room.id', ondelete='CASCADE'), primary_key=True)
+    user_id: Mapped[str] = mapped_column(ForeignKey('user.id', ondelete='CASCADE'), primary_key=True)
+    role: Mapped[RoomRole] = mapped_column(default=RoomRole.MEMBER)
+    room: Mapped['Room'] = relationship(back_populates='members')
+    user: Mapped['User'] = relationship(back_populates='room_members')
