38ef04e80c4eda1192fa0a7f3059ad60377775bb
RomesAll <romesky845@gmail.com>
2026-09-21 21:03:17 +0300

    feat(#5): добавил init файл с импортами

---
+++ app/data_layer/models/__init__.py
@@ -0,0 +1,24 @@
+from .messages import Message, MessageAttachment, MessageType
+from .base import Base
+from .rooms import Room, RoomMember, RoomRole
+from .chats import Chat, ChatMember, ChatType
+from .users import User, UserInfo, RoleEnum
+
+
+__author__ = 'RomesAll'
+__version__ = 'v0.1.0'
+__all__ = [
+    'Base',
+    'Message',
+    'MessageAttachment',
+    'MessageType',
+    'Room',
+    'RoomMember',
+    'RoomRole',
+    'Chat',
+    'ChatMember',
+    'ChatType',
+    'User',
+    'UserInfo',
+    'RoleEnum'
+]
