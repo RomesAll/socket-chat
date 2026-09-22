25ef6cba1ac34e716c9590f4168a2c0724a5f6f2
RomesAll <romesky845@gmail.com>
2026-09-22 13:03:16 +0300

    feat(#5): добавил dto для работы с пользователем

---
+++ app/shared/dto/user.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+from pydantic import BaseModel
+from app.data_layer.models import RoleEnum
+
+
+class UserDtoSave(BaseModel):
+    """DTO для хранения и валидации информации о новом пользователе"""
+    id: str
+    display_name: str
+    avatar_url: Path | None = None
+    user_info: UserDtoInfoSave
+
+
+class UserDtoInfoSave(BaseModel):
+    """DTO для хранения и валидации расширенной информации о новом пользователе"""
+    id: str
+    description: str | None = None
+    years_old: int | None = None
+    role: RoleEnum = RoleEnum.DEFAULT_USER
+    email: str
+    password: bytes
+
+
+class UserDtoUpdate(BaseModel):
+    """DTO для хранения и валидации информации об обновлении сущ. пользователя"""
+    display_name: str | None = None
+    avatar_url: Path | None = None
+    description: str | None = None
+    years_old: int | None = None
+    role: RoleEnum | None = None
