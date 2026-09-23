dc2072a52f6848f702f34263051312ce3c0ef476
RomesAll <romesky845@gmail.com>
2026-09-23 13:18:37 +0300

    feat(#5): добавил init файл для репозиториев

---
+++ app/data_layer/repositories/__init__.py
@@ -0,0 +1,16 @@
+from .user_repo import UserRepository
+from .base import BaseRepository
+from .room_repo import RoomRepository
+from .chat_repo import ChatRepository
+from .message_repo import MessageRepository
+
+
+__author__ = 'RomesAll'
+__version__ = 'v0.1.0'
+__all__ = [
+    'BaseRepository',
+    'UserRepository',
+    'RoomRepository',
+    'ChatRepository',
+    'MessageRepository'
+]
