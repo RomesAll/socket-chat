84bfcc90b632558ee400eefc5f08d460c9f98163
RomesAll <romesky845@gmail.com>
2026-09-23 11:57:13 +0300

    feat(#5): добавил репозитория для работы с пользователями и dto для методов

--- app/data_layer/repositories/user_repo.py
+++ app/data_layer/repositories/user_repo.py
@@ -1,119 +1,59 @@
-from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 from sqlalchemy.orm import joinedload, selectinload
-from app.shared.dto.user import UserDtoSave, UserDtoUpdate
+from app.data_layer.repositories.base import BaseRepository
+from app.shared.dto.user import UserDtoUpdateDefaultInfo, UserDtoUpdateExtendedInfo, UserDtoInfoSave
 from app.data_layer.models import *
 
 
-class UserRepository:
-    USER_MODEL = User
+class UserRepository(BaseRepository[User, UserDtoInfoSave, UserDtoUpdateDefaultInfo]):
+    MODEL = User
     USER_INFO_MODEL = UserInfo
 
-    def __init__(self, session: AsyncSession):
-        self.session = session
+    async def get_user(self, with_relation: bool = False) -> list[User]:
+        options = None
+        if with_relation:
+            options = self._with_user_options()
+        result = await self._get(options)
+        return result
 
-    async def save(self, user_save: UserDtoSave) -> User:
-        """
-        Сохранение информации о пользователе в БД
-        :param user_save: dto объект
-        :return:
-        """
-        user_info = user_save.user_info.model_dump(exclude_none=True)
-        user = self.USER_MODEL(
-            id=user_save.id,
-            display_name=user_save.display_name,
-            avatar_url=user_save.avatar_url,
-            user_info=self.USER_INFO_MODEL(**user_info)
+    async def get_user_by_id(self, user_id: str, with_relation: bool = False) -> User:
+        options = None
+        if with_relation:
+            options = self._with_user_options()
+        result = await self._get_by_id(user_id, options)
+        return result
+
+    async def get_user_by_email(self, email: str, with_relation: bool = False) -> UserInfo:
+        stmt = (
+            select(self.USER_INFO_MODEL)
+            .where(self.USER_INFO_MODEL.email == email)
         )
-        self.session.add(user)
-        await self.session.flush()
-        return user
+        if with_relation:
+            stmt = stmt.options(*self._with_user_options())
+        sqla_obj = await self.session.execute(stmt)
+        user_info = sqla_obj.scalar_one_or_none()
+        if not user_info:
+            raise Exception
+        return user_info
 
-    async def update(self, user_id: str, user_update: UserDtoUpdate) -> User:
-        """
-        Обновление информации о пользователе в БД
-        :param user_id: id пользователя
-        :param user_update: dto объект
-        :return:
-        """
-        update_data = user_update.model_dump(
-            exclude_none=True, exclude_unset=True, exclude_defaults=True
+    async def update_default_info(self, user_id, update_user: UserDtoUpdateDefaultInfo) -> User:
+        result = await self.update(user_id, update_user)
+        return result
+
+    async def update_extended_info(self, user_id, update_user: UserDtoUpdateExtendedInfo) -> UserInfo:
+        user = await self.get_user_by_id(user_id, True)
+        raw_data = update_user.model_dump(
+            exclude_none=True,
+            exclude_unset=True,
+            exclude_defaults=True
         )
-        user = await self.get_by_id(user_id, True)
-        self._update(update_data, user)
-        self._update(update_data, user.user_info)
-        return user
+        for k, v in raw_data.items():
+            setattr(user.user_info, k, v)
+        return user.user_info
 
-    @staticmethod
-    def _update(update_data: dict, user: Base | None = None):
-        """Обновление атрибутов дочерних объектов Base, без учета вложенных"""
-        if not user:
-            raise Exception
-        user_columns = user.__table__.columns.keys()
-        for col, val in update_data.items():
-            if col in user_columns:
-                setattr(user, col, val)
-
-    async def delete(self, user_id: str) -> User:
-        """
-        Удаление пользователя из БД
-        :param user_id: id пользователя
-        :return:
-        """
-        user = await self.get_by_id(user_id, True)
-        await self.session.delete(user)
-        return user
-
-    async def get(self, with_relation: bool = False) -> list[User]:
-        """
-        Получение списка пользователей из БД
-        :param with_relation: включать ли relationship (делает доп. запросы)
-        :return:
-        """
-        stmt = select(self.USER_MODEL)
-        if with_relation:
-            stmt = stmt.options(
-                joinedload(self.USER_MODEL.user_info),
-                selectinload(self.USER_MODEL.room_members),
-                selectinload(self.USER_MODEL.owned_rooms),
-            )
-        result = await self.session.execute(stmt)
-        return list(result.scalars().all())
-
-    async def get_by_id(self, user_id: str, with_relation: bool = False) -> User:
-        """
-        Получение информации о пользователе по id
-        :param user_id: id пользователя
-        :param with_relation: включать ли relationship (делает доп. запросы)
-        :return:
-        """
-        stmt = select(self.USER_MODEL).where(self.USER_MODEL.id == user_id)
-        if with_relation:
-            stmt = stmt.options(
-                joinedload(self.USER_MODEL.user_info),
-                selectinload(self.USER_MODEL.room_members),
-                selectinload(self.USER_MODEL.owned_rooms),
-            )
-        result = await self.session.execute(stmt)
-        user = result.scalar_one_or_none()
-        if not user:
-            raise Exception
-        return user
-
-    async def get_by_email(self, user_email: str, with_relation: bool = False) -> UserInfo:
-        """
-        Получение информации о пользователе по email
-        :param user_email: email пользователя
-        :param with_relation: включать ли relationship (делает доп. запросы)
-        :return:
-        """
-        stmt = select(self.USER_INFO_MODEL).where(self.USER_INFO_MODEL.email == user_email)
-        if with_relation:
-            stmt = stmt.options(
-                joinedload(self.USER_MODEL.user_info),
-            )
-        result = await self.session.execute(stmt)
-        user = result.scalar_one_or_none()
-        if not user:
-            raise Exception
-        return user
+    def _with_user_options(self):
+        return {
+            joinedload(self.MODEL.user_info),
+            selectinload(self.MODEL.room_members),
+            selectinload(self.MODEL.owned_rooms)
+        }
--- app/shared/dto/user.py
+++ app/shared/dto/user.py
@@ -1,4 +1,3 @@
-from pathlib import Path
 from pydantic import BaseModel
 from app.data_layer.models import RoleEnum
 
@@ -7,7 +6,7 @@ class UserDtoSave(BaseModel):
     """DTO для хранения и валидации информации о новом пользователе"""
     id: str
     display_name: str
-    avatar_url: Path | None = None
+    avatar_url: str | None = None
     user_info: UserDtoInfoSave
 
 
@@ -21,10 +20,14 @@ class UserDtoInfoSave(BaseModel):
     password: bytes
 
 
-class UserDtoUpdate(BaseModel):
+class UserDtoUpdateDefaultInfo(BaseModel):
     """DTO для хранения и валидации информации об обновлении сущ. пользователя"""
     display_name: str | None = None
-    avatar_url: Path | None = None
+    avatar_url: str | None = None
+
+
+class UserDtoUpdateExtendedInfo(BaseModel):
+    """DTO для обновления расширенной информации о пользователе"""
     description: str | None = None
     years_old: int | None = None
     role: RoleEnum | None = None
