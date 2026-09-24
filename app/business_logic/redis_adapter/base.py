5b45ba21989134cbb6203b207fefaeb98182f00e
RomesAll <romesky845@gmail.com>
2026-09-24 15:21:17 +0300

    feat(#8): добавил метакласс для создания одиночек (singleton) потокобезопасный и обновил класс создания подключения к redis

--- app/business_logic/redis_adapter/base.py
+++ app/business_logic/redis_adapter/base.py
@@ -1,64 +1,41 @@
-from typing import Self
-from redis.asyncio import *
-from functools import wraps
-from redis.exceptions import ConnectionError, TimeoutError, AuthenticationError, ResponseError, DataError
-from app.business_logic.exceptions import RedisConnectionError, AuthRedisError, ResponseRedisError, DataErrorRedis
+from redis.asyncio import Redis
+from threading import Lock
 
 
-class BaseRedisAdapter:
-    """Базовый класс адаптер для redis"""
-    def __init__(self, url: str):
-        self.url = url
-        self._client: Redis | None = None
+class RedisConnectionSingleton(type):
+    """
+    Паттерн Singleton (потокобезопасный) для создания одиночек
+    """
+    _instances = {}
+    _lock = Lock()
 
-    @staticmethod
-    def exception_handler(generate_exc: bool = False):
-        """Обработчик redis исключений"""
-        def wrapper_func(func):
-            @wraps(func)
-            async def wrapper(self, *args, **kwargs):
-                exc = None
-                try:
-                    result = func(self, *args, **kwargs)
-                    return result
-                except AuthenticationError as e:
-                    exc = AuthRedisError(self.url, str(e))
-                except ConnectionError as e:
-                    exc = RedisConnectionError(self.url, str(e))
-                except TimeoutError as e:
-                    exc = RedisConnectionError(self.url, str(e))
-                except ResponseError as e:
-                    exc = ResponseRedisError(str(e))
-                except DataError as e:
-                    exc = DataErrorRedis(str(e))
-                finally:
-                    if exc and generate_exc:
-                        raise exc
-            return wrapper
-        return wrapper_func
+    def __call__(cls, *args, **kwargs):
+        with cls._lock:
+            if cls not in cls._instances:
+                cls._instances[cls] = super().__call__(*args, **kwargs)
+        return cls._instances[cls]
 
-    async def connection(self) -> Self:
-        """Создание подключения к redis"""
-        if not self._client:
-            try:
-                self._client = from_url(self.url)
-                await self._client.ping()
-            except (ConnectionError, ValueError) as e:
-                self._client = None
-                raise e
-        return self
 
-    async def close(self) -> Self:
-        """Отключение от redis сервера"""
+class RedisConnection(metaclass=RedisConnectionSingleton):
+    """
+    Класс создания подключения к redis серверу
+    """
+    _client: Redis | None = None
+
+    async def connection(self, url: str) -> Redis:
+        if self._client is None:
+            client = Redis.from_url(url)
+            await client.ping()
+            self._client = client
+        return self._client
+
+    async def close(self) -> None:
         if self._client:
             await self._client.close()
             self._client = None
-        return self
 
     @property
     def client(self) -> Redis:
-        """Получение клиента"""
-        if not self._client:
-            raise RuntimeError("Redis client не инициализирован. Сначала вызовите connect()")
+        if self._client is None:
+            raise RuntimeError("Redis client не инициализирован")
         return self._client
-
