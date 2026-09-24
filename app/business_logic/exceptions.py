345cc95a60756b58803f03fcdb816dc92c31a373
RomesAll <romesky845@gmail.com>
2026-09-24 15:21:42 +0300

    feat(#8): добавил пользовательские исключения для redis

--- app/business_logic/exceptions.py
+++ app/business_logic/exceptions.py
@@ -1,3 +1,5 @@
+from uuid import UUID
+
 
 class RedisConnectionError(Exception):
     """Сервер Redis выключен, упал, или указан неверный хост/порт"""
@@ -30,4 +32,30 @@ class DataErrorRedis(Exception):
     def __init__(self, cause: str):
         self.cause = cause
         self.msg = f'Ошибка валидации данных на стороне Python, причина: {self.cause}'
+        super().__init__(self.msg)
+
+
+class SaveIdRefreshTokenWhiteListError(Exception):
+    """Ошибка сохранения id refresh токена"""
+    def __init__(self, user_id: str):
+        self.user_id = user_id
+        self.msg = f'Ошибка сохранения id refresh токена для пользователя {user_id}'
+        super().__init__(self.msg)
+
+
+class SessionKeyNotFound(Exception):
+    """Не удалось найти сессионный ключ"""
+    def __init__(self, user_id: str, session_id: UUID):
+        self.user_id = user_id
+        self.session_id = session_id
+        self.msg = f'Не удалось найти id сессионного ключа {session_id} для пользователя {user_id}'
+        super().__init__(self.msg)
+
+
+class SaveSessionKeyError(Exception):
+    """Ошибка сохранения сессионного ключа"""
+    def __init__(self, user_id: str, session_id: UUID):
+        self.user_id = user_id
+        self.session_id = session_id
+        self.msg = f'Не удалось сохранить сессионный ключ {session_id} для пользователя {user_id}'
         super().__init__(self.msg)
