11fa734bc964fac53da20d721df8c0f3f2cf5e59
RomesAll <romesky845@gmail.com>
2026-09-25 20:51:44 +0300

    refactor: добавил доп события в enum и изменил название data на payload

--- app/data_layer/models/events.py
+++ app/data_layer/models/events.py
@@ -7,9 +7,11 @@ from pydantic import Field
 
 class EventType(str, Enum):
     """Перечисление типов событий"""
-    SEND_EMAIL = 'send_success_code'
+    SEND_EMAIL = 'send_email'
     SEND_MESSAGE = 'send_message'
-
+    NEW_USER = 'new_user'
+    UPDATE_USER = 'update_user'
+    DELETE_USER = 'delete_user'
 
 
 class Status(str, Enum):
@@ -26,7 +28,7 @@ class OutboxEvent(MongoBaseModel):
 
     id: str = Field(default_factory=lambda: str(uuid4()), alias="_id")
     event_name: str = Field(..., description="Название события, например: MessageSentEvent")
-    data: dict = Field(..., description="Полезная нагрузка (payload) события в виде JSON/dict")
+    payload: dict = Field(..., description="Полезная нагрузка (payload) события в виде JSON/dict")
     status: Status = Field(default=Status.NEW, description="Статус: NEW, PROCESSED, FAILED")
     type: EventType = Field(..., description="Тип события")
     created_at: datetime = Field(default_factory=datetime.utcnow)
